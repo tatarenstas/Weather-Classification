8b5c4d713c2eafaf57e7a9628bd2d9202523d8b3
Stanislav Tataren <79329397+tatarenstas@users.noreply.github.com>
2022-01-09 01:38:52 +0200

    Update train.py

--- train.py
+++ train.py
@@ -4,8 +4,6 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
-img_width=256
-img_height=256
 TRAINING_DIR = 'dataset/train/'
 VALIDATION_DIR = 'dataset/validation/'
 
@@ -16,15 +14,15 @@ train_datagen = ImageDataGenerator(rescale = 1/255.0,
 train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
                                                     batch_size=16,
                                                     class_mode='categorical',
-                                                    target_size=(img_height, img_width))
+                                                    target_size=(256, 256))
 validation_datagen = ImageDataGenerator(rescale = 1/255.0)
 validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
                                                               batch_size=16,
                                                               class_mode='categorical',
-                                                              target_size=(img_height, img_width))
+                                                              target_size=(256, 256))
 
 model = Sequential([
-    Conv2D(16, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+    Conv2D(16, (3, 3), activation='relu', input_shape=(256, 256, 3)),
     MaxPooling2D(2, 2),
     Conv2D(32, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
