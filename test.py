f1a47e42146da202907378deb11505d04e7b30bd
Stanislav Tataren <79329397+tatarenstas@users.noreply.github.com>
2022-01-09 01:36:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,11 @@ import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 import matplotlib.pyplot as plt
 
-img_width=256
-img_height=256
-weather = ['Cloudy','Foggy','Rainy','Shine','Sunrise']
 model = keras.models.load_model('model.h5')
+weather = ['Cloudy','Foggy','Rainy','Shine','Sunrise']
 
 def preprocess_image(path):
-    img = load_img(path, target_size = (img_height, img_width))
+    img = load_img(path, target_size = (256, 256))
     a = img_to_array(img)
     a = np.expand_dims(a, axis = 0)
     a /= 255.0
