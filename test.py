fa4abcbd95035321dbb4056a6e9bc9ec26b25a65
Stanislav Tataren <79329397+tatarenstas@users.noreply.github.com>
2022-01-09 01:17:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 from tensorflow import keras
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
+
+img_width=256
+img_height=256
+weather = ['Cloudy','Foggy','Rainy','Shine','Sunrise']
 model = keras.models.load_model('model.h5')
 
 def preprocess_image(path):
